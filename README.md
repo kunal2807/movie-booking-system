f7d8a3ed6627495dac3c03391263d13ae9739ec9
Chad Hinds <chadrickj8@gmail.com>
2020-10-06 11:20:07 -0400

    Add new README with credit and link to course

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pro Shop
+
+This project is based on Brad Traversy's [Udemy course] (https://www.udemy.com/course/mern-ecommerce) using the MERN stack.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Prop Shop</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
