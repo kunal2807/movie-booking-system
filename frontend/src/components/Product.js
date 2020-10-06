4db9eb37eb48486c4893d476bdb163cb95fb35f2
Chad Hinds <chadrickj8@gmail.com>
2020-10-06 11:32:57 -0400

    add price and product ratings

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,11 +3,23 @@ import { Card } from 'react-bootstrap'
 
 const Product = ({ product }) => {
     return (
-        <Card className="my-3 p-3 rounded">
-            <a href={`/products/${product._id}`}>
-                <Card.Img src={product.image} />
-            </a>
-        </Card>
+        <>
+            <Card className="my-3 p-3 rounded">
+                <a href={`/products/${product._id}`}>
+                    <Card.Img src={product.image} />
+                </a>
+            </Card>
+
+            <Card.Body className="my-3 p-3 rounded">
+                <a href={`/products/${product._id}`}>
+                    <Card.Title as="div"><strong>{product.name}</strong></Card.Title>
+                </a>
+                <Card.Text as="div">
+                    <div className="my-3">{product.rating} from {product.numReviews}</div>
+                </Card.Text>
+                <Card.Text as="h3">${product.price}</Card.Text>
+            </Card.Body>
+        </>
     );
 };
 
