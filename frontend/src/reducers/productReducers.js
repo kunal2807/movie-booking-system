79551203b96cc76068398c78f36a55c433a03948
Chad Hinds <chadrickj8@gmail.com>
2020-10-09 15:40:16 -0400

    More reducer setup

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,16 +1,20 @@
+import {
+    PRODUCTS_LIST_REQUEST,
+    PRODUCTS_LIST_FAIL,
+    PRODUCTS_LIST_SUCCESS
+} from '../constants/productConstants'
+
+
 // Takes in the initial state and the action that determines how state should update
 export const productListReducer = (initialState = { products: [] }, action) => {
     switch (action.type) {
-        case 'PRODUCTS_LIST_REQUEST':
-            return { loading: true }
+        case PRODUCTS_LIST_REQUEST:
+            return { loading: true, products: [] }
 
-        case 'PRODUCTS_LIST_SUCCESS':
-            return { loading: false }
+        case PRODUCTS_LIST_SUCCESS:
+            return { loading: false, products: action.payload }
 
-        case 'PRODUCTS_LIST_SUCCESS':
-            return { loading: false }
-
-        case 'PRODUCTS_LIST_FAIL':
+        case PRODUCTS_LIST_FAIL:
             return { loading: false, error: action.payload }
         default:
             return initialState
