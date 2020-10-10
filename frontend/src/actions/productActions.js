1df9cea5609a1c41cd8b0a4896782c485f2ab1b9
Chad Hinds <chadrickj8@gmail.com>
2020-10-10 13:41:10 -0400

    Fix up actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,3 +3,23 @@ import {
     PRODUCTS_LIST_FAIL,
     PRODUCTS_LIST_SUCCESS
 } from '../constants/productConstants'
+import axios from 'axios';
+// Actions
+export const list = () => async (dispatch) => {
+    try {
+        dispatch({
+            type: PRODUCTS_LIST_REQUEST
+        })
+        const { data } = await axios.get('/api/products')
+        dispatch({
+            type: PRODUCTS_LIST_SUCCESS,
+            payload: data
+        })
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCTS_LIST_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
