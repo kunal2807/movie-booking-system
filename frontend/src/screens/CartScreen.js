6dd242cc1ffb7dc6fbc927f9292900cb34d8008a
Chad Hinds <chadrickj8@gmail.com>
2020-10-13 11:11:57 -0400

    Add cartItems from state to cart component

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -6,6 +6,20 @@ import { addToCart } from '../actions/cartActions';
 import { Col, Row, ListGroup, Image, Button, Form, Card } from 'react-bootstrap'
 
 const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id
+
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    const dispatch = useDispatch()
+    const cart = useSelector(state => state.cart)
+
+    const { cartItems } = cart
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
     return (
         <div>
             {'Stuff goes here'}
