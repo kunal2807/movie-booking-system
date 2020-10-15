6ca14399a284716d96e876722bd0cb269766a3e8
Chad Hinds <chadrickj8@gmail.com>
2020-10-15 12:47:08 -0400

    Middleware for new user password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -29,6 +29,14 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
     // compare plain text to encrypted password
     return await bcrypt.compare(enteredPassword, this.password)
 }
+// Middleware for new user password
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password)
+})
 
 const User = mongoose.model('User', userSchema)
 export default User
