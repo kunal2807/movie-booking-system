2624601f5a4113cde96c229c4f1d92db7ab02803
Chad Hinds <chadrickj8@gmail.com>
2020-10-15 12:23:14 -0400

    Fix typo that had JWT as TWT

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.TWT_SECRET, {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: '31d'
     })
 }
