4663702da98fc9515565c006a1cc94179a1ec972
Chad Hinds <chadrickj8@gmail.com>
2020-10-20 18:50:18 -0400

    More shipping setup

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+
+const ShippingScreen = () => {
+    return (
+        <div>
+            Ship
+        </div>
+    );
+};
+
+export default ShippingScreen;
