f0b539e31d1d2a775bac8c9c0a89ce4226344f4c
Chad Hinds <chadrickj8@gmail.com>
2020-10-20 19:02:26 -0400

    Third iteration of shipping

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -5,11 +5,59 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 
-const ShippingScreen = () => {
+const ShippingScreen = ({ history }) => {
+    const [address, setAddress] = useState('')
+    const [city, setCity] = useState('')
+    const [postalCode, setPostalCode] = useState('')
+    const [country, setCountry] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submit ')
+    }
     return (
-        <div>
-            Ship
-        </div>
+        <FormContainer>
+            <h1> Shipping</h1>
+            <form onSubmit={submitHandler}>
+                <Form.Group controlId='address'>
+                    <Form.Label>Adress</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter address'
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='city'>
+                    <Form.Label>City</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter city'
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    ></Form.Control>
+                </Form.Group> <Form.Group controlId='postalCode'>
+                    <Form.Label>Postal Code</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter postalCode'
+                        value={postalCode}
+                        onChange={(e) => setPostalCode(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='country'>
+                    <Form.Label>Country</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter country'
+                        value={country}
+                        onChange={(e) => setAddress(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+                <Button type="submit" variant='primary'>Continue</Button>
+            </form>
+
+        </FormContainer>
     );
 };
 
