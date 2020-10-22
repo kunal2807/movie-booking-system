4b5e19338fb184d4a0eb79660455a807f81f7e81
Chad Hinds <chadrickj8@gmail.com>
2020-10-22 11:02:45 -0400

    Add order controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,20 +2,21 @@ import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js';
 
-//Auth user @ACCESS: PUBLIC
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
 
-    // Find user by email and password
     const user = await User.findOne({ email })
 
-    if (user && await (user.matchPassword(password))) {
+    if (user && (await user.matchPassword(password))) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: generateToken(user._id)
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
@@ -23,24 +24,26 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-
-    // Find user by email and password
     const user = await User.findById(req.user._id)
-
+    console.log(req.user._id)
     if (user) {
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
-            isAdmin: user.isAdmin
+            isAdmin: user.isAdmin,
         })
     } else {
-        res.status(401)
-        throw new Error('Invalid email or password')
+        res.status(404)
+        throw new Error('User not found')
     }
 })
-// NEW USER POST api/users
+
+//POST NEW USER api/users
 const registerUser = asyncHandler(async (req, res) => {
     const { email, password, name } = req.body
 
@@ -70,16 +73,20 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('Invalid user data')
     }
 })
-// Update User profile PUT api/user/profile. Private
-const updateUserProfile = asyncHandler(async (req, res) => {
 
-    // Find user by email and password
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id)
 
     if (user) {
-        user.name = req.body.name || username
-        user.email = req.body.email || email
-        if (req.body.password) { user.password = req.body.password }
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if (req.body.password) {
+            user.password = req.body.password
+        }
+
         const updatedUser = await user.save()
 
         res.json({
@@ -87,11 +94,11 @@ const updateUserProfile = asyncHandler(async (req, res) => {
             name: updatedUser.name,
             email: updatedUser.email,
             isAdmin: updatedUser.isAdmin,
-            token: generateToken(updatedUser._id)
+            token: generateToken(updatedUser._id),
         })
     } else {
-        res.status(401)
-        throw new Error('Invalid email or password')
+        res.status(404)
+        throw new Error('User not found')
     }
 })
 
