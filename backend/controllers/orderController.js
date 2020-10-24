bcfe5d6b821070fbc481c94995bc6a3c9f69c068
Chad Hinds <chadrickj8@gmail.com>
2020-10-24 19:35:29 -0400

    Add update order function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,37 +1,74 @@
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js'
 
-//  CREATE NEW ORDER
-//  POST /api/orders
-
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
     const {
-        orderItems, shippingAddress, paymentMethod,
-        itemsPrice, taxPrice, shippingPrice, totalPrice
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
     } = req.body
-
+    console.log(shippingAddress)
     if (orderItems && orderItems.length === 0) {
         res.status(400)
         throw new Error('No order items')
         return
     } else {
         const order = new Order({
-            orderItems, shippingAddress, paymentMethod,
-            itemsPrice, taxPrice, shippingPrice, totalPrice
+            orderItems,
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
         })
+
         const createdOrder = await order.save()
+        console.log(createdOrder._id)
         res.status(201).json(createdOrder)
     }
 })
 
-// Get order by Id
-const getOrderById = asyncHandler(async (req, res) => {
-    const order = await (await Order.findById(req.params.id)).populate('user', 'name email')
 
-    if (order) res.json(order)
-    else {
+// Get order by Id
+// GET /api/orders
+// @desc    Get order by ID
+// @route   GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate(
+        'user',
+        'name email'
+    )
+
+    if (order) {
+        res.json(order)
+    } else {
         res.status(404)
-        throw new Error('order not found')
+        throw new Error('Order not found')
     }
 })
-export { addOrderItems, getOrderById }
+
+// Update order to piad
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPiad = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        res.json(order)
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPiad }
