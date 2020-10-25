44800b4d96e56918963a4b190204ea8425b9c63f
Chad Hinds <chadrickj8@gmail.com>
2020-10-25 19:08:19 -0400

    Order update changes to put request instead of get

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems, getMyOrders, getOrderById, updateOrderToPiad } from '../controllers/orderController.js';
+import { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById);
-router.route('/:id/pay').get(protect, updateOrderToPiad);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default router;
