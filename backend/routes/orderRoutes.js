e254c60b341e2ab7f60568912b95cee02fdedbed
Chad Hinds <chadrickj8@gmail.com>
2020-10-25 12:20:09 -0400

    Paypal support for payments

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
-import { protect } from '../middleware/authMiddleware.js'
+import express from 'express';
+const router = express.Router();
+import { addOrderItems, getOrderById, updateOrderToPiad } from '../controllers/orderController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
-const router = express.Router()
+router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').get(protect, updateOrderToPiad);
 
-
-router.route('/').post(protect, addOrderItems)
-router.route('/:id').post(protect, getOrderById)
-export default router
+export default router;
