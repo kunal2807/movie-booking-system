0ac0230ce0f48c6f3063b6f79e0b19f6bead93c3
Chad Hinds <chadrickj8@gmail.com>
2020-10-26 12:44:07 -0400

    Adjust the order model

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,22 +57,24 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
-// Update order to paid
+// @desc    Update order to paid
 // @route   GET /api/orders/:id/pay
 // @access  Private
-const updateOrderToPiad = asyncHandler(async (req, res) => {
+const updateOrderToPaid = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id)
 
     if (order) {
-        order.isPiad = true
+        order.isPaid = true
         order.paidAt = Date.now()
         order.paymentResult = {
             id: req.body.id,
             status: req.body.status,
             update_time: req.body.update_time,
-            email_address: req.body.payer.email_address
+            email_address: req.body.payer.email_address,
         }
+
         const updatedOrder = await order.save()
+
         res.json(updatedOrder)
     } else {
         res.status(404)
@@ -88,4 +90,4 @@ const getMyOrders = asyncHandler(async (req, res) => {
     res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPiad, getMyOrders }
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
