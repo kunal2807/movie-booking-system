a8594f6c3084a37bbd7a2e7befb5f3967142cf36
Chad Hinds <chadrickj8@gmail.com>
2020-11-10 10:04:28 -0500

    Add product create and update reducers

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,7 @@
 import {
     PRODUCTS_LIST_REQUEST,
     PRODUCTS_LIST_FAIL,
-    PRODUCTS_LIST_SUCCESS, PRODUCTS_DETAILS_REQUEST, PRODUCTS_DETAILS_SUCCESS, PRODUCTS_DETAILS_FAIL, PRODUCTS_DELETE_REQUEST, PRODUCTS_DELETE_SUCCESS, PRODUCTS_DELETE_FAIL
+    PRODUCTS_LIST_SUCCESS, PRODUCTS_DETAILS_REQUEST, PRODUCTS_DETAILS_SUCCESS, PRODUCTS_DETAILS_FAIL, PRODUCTS_DELETE_REQUEST, PRODUCTS_DELETE_SUCCESS, PRODUCTS_DELETE_FAIL, PRODUCTS_CREATE_REQUEST, PRODUCTS_CREATE_SUCCESS, PRODUCTS_CREATE_FAIL, PRODUCTS_CREATE_RESET, PRODUCTS_UPDATE_REQUEST, PRODUCTS_UPDATE_SUCCESS, PRODUCTS_UPDATE_FAIL, PRODUCTS_UPDATE_RESET
 } from '../constants/productConstants'
 
 
@@ -50,3 +50,32 @@ export const productDeleteReducer = (state = {}, action) => {
     }
 }
 
+export const productCreateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case PRODUCTS_CREATE_REQUEST:
+            return { loading: true }
+        case PRODUCTS_CREATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload }
+        case PRODUCTS_CREATE_FAIL:
+            return { loading: false, error: action.payload }
+        case PRODUCTS_CREATE_RESET:
+            return {}
+        default:
+            return state
+    }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+        case PRODUCTS_UPDATE_REQUEST:
+            return { loading: true }
+        case PRODUCTS_UPDATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload }
+        case PRODUCTS_UPDATE_FAIL:
+            return { loading: false, error: action.payload }
+        case PRODUCTS_UPDATE_RESET:
+            return { product: {} }
+        default:
+            return state
+    }
+}
