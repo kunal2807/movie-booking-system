3ac59ea10a0feabc529ad8cecdc56155bd66e992
Chad Hinds <chadrickj8@gmail.com>
2020-11-10 10:06:34 -0500

    Add product create and update reducers

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -97,3 +97,82 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
         })
     }
 }
+
+export const createProduct = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCTS_CREATE_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        const { data } = await axios.post(`/api/products`, {}, config)
+
+        dispatch({
+            type: PRODUCTS_CREATE_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        const message =
+            error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        if (message === 'Not authorized, token failed') {
+            dispatch(logout())
+        }
+        dispatch({
+            type: PRODUCTS_CREATE_FAIL,
+            payload: message,
+        })
+    }
+}
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCTS_UPDATE_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        const { data } = await axios.put(
+            `/api/products/${product._id}`,
+            product,
+            config
+        )
+
+        dispatch({
+            type: PRODUCTS_UPDATE_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        const message =
+            error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message
+        if (message === 'Not authorized, token failed') {
+            dispatch(logout())
+        }
+        dispatch({
+            type: PRODUCTS_UPDATE_FAIL,
+            payload: message,
+        })
+    }
+}
