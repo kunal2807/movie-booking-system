c809872c5b5bc8eac17ec0ecea4633079513026c
Chad Hinds <chadrickj8@gmail.com>
2020-12-03 14:43:17 -0500

    product search on backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,20 +1,41 @@
-import Product from '../models/productModel.js'
 import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
 
-// Fetch all products @ACCESS: PUBLIC
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({})
+    const pageSize = 10
+    const page = Number(req.query.pageNumber) || 1
 
-    res.json(products)
+    const keyword = req.query.keyword
+        ? {
+            name: {
+                $regex: req.query.keyword,
+                $options: 'i',
+            },
+        }
+        : {}
+
+    const count = await Product.countDocuments({ ...keyword })
+    const products = await Product.find({ ...keyword })
+        .limit(pageSize)
+        .skip(pageSize * (page - 1))
+
+    res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
 const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
+
     if (product) {
         res.json(product)
     } else {
         res.status(404)
-        throw new Error("Product not Found!")
+        throw new Error('Product not found')
     }
 })
 
@@ -38,19 +59,19 @@ const deleteProduct = asyncHandler(async (req, res) => {
 // @access  Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
     const product = new Product({
-        name: 'Sample Nmae',
+        name: 'Sample name',
         price: 0,
         user: req.user._id,
         image: '/images/sample.jpg',
-        brand: 'Sample Brand',
-        category: 'Category',
+        brand: 'Sample brand',
+        category: 'Sample category',
         countInStock: 0,
         numReviews: 0,
-        description: 'Sample description'
+        description: 'Sample description',
     })
 
     const createdProduct = await product.save()
-    res.status(201).json(product)
+    res.status(201).json(createdProduct)
 })
 
 // @desc    Update a product
@@ -112,7 +133,7 @@ const createProductReview = asyncHandler(async (req, res) => {
         }
 
         product.reviews.push(review)
-        // update the number of reviews
+
         product.numReviews = product.reviews.length
 
         product.rating =
@@ -127,4 +148,21 @@ const createProductReview = asyncHandler(async (req, res) => {
     }
 })
 
-export { getProductById, getProducts, deleteProduct, createProduct, updateProduct, createProductReview }
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+
+    res.json(products)
+})
+
+export {
+    getProducts,
+    getProductById,
+    deleteProduct,
+    createProduct,
+    updateProduct,
+    createProductReview,
+    getTopProducts,
+}
