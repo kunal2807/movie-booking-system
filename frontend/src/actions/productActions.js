09b5f4c92ba7c24b494e04e2d9f49e7c5aa4969c
Chad Hinds <chadrickj8@gmail.com>
2020-12-02 22:56:46 -0500

    update listProducts action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -23,7 +23,7 @@ import {
 import axios from 'axios';
 import { logout } from './userActions'
 
-// Actions are here. Fake
+// Actions are here. 
 export const listProducts = (keyword = '', pageNumber = '') => async (
     dispatch
 ) => {
