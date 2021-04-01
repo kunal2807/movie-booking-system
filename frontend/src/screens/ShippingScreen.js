2c9b4305a3dda6d132e61c82a87df1eaf1878b08
kunal2807 <jagtiani.kunal2000@gmail.com>
2021-04-01 18:50:49 +0530

    changed

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
+import DropdownMultiselect from 'react-multiselect-dropdown-bootstrap'
 
 const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart)
@@ -13,6 +14,7 @@ const ShippingScreen = ({ history }) => {
   const [city, setCity] = useState(' ')
   const [postalCode, setPostalCode] = useState(' ')
   const [country, setCountry] = useState(' ')
+  const [opts, setOpts] = useState('select')
 
   const dispatch = useDispatch()
 
@@ -21,6 +23,57 @@ const ShippingScreen = ({ history }) => {
     dispatch(saveShippingAddress({ address, city, postalCode, country }))
     history.push('/payment')
   }
+  let options = [
+    'select',
+    'A1',
+    'A2',
+    'A3',
+    'A4',
+    'A5',
+    'A6',
+    'A7',
+    'A8',
+    'A9',
+    'A10',
+    'A11',
+    'A12',
+    'B1',
+    'B2',
+    'B3',
+    'B4',
+    'B5',
+    'B6',
+    'B7',
+    'B8',
+    'B9',
+    'B10',
+    'B11',
+    'B12',
+    'C1',
+    'C2',
+    'C3',
+    'C4',
+    'C5',
+    'C6',
+    'C7',
+    'C8',
+    'C9',
+    'C10',
+    'C11',
+    'C12',
+    'D1',
+    'D2',
+    'D3',
+    'D4',
+    'D5',
+    'D6',
+    'D7',
+    'D8',
+    'D9',
+    'D10',
+    'D11',
+    'D12',
+  ]
 
   return (
     <FormContainer>
@@ -70,7 +123,10 @@ const ShippingScreen = ({ history }) => {
             onChange={(e) => setCountry(e.target.value)}
           ></Form.Control>
         </Form.Group> */}
-
+        <Form.Group controlId='seats'>
+          <Form.Label>Choose Seats</Form.Label>
+          <DropdownMultiselect options={options} name='seats' />
+        </Form.Group>
         <Button type='submit' variant='primary'>
           Continue
         </Button>
